e915e8b69409cabbbf335d40f90cb22dc922e0c4
bensimonlidor <47791397+bensimonlidor@users.noreply.github.com>
2019-02-26 02:40:08 +0200

    Create targil6.cs

---
+++ targil6.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int a;
+        int atz=1;
+        int i=2;
+         a = Convert.ToInt32(Console.ReadLine());
+         while (i<=a)
+         {
+         atz=atz*i;
+         i++;
+         }
+         else
+         {
+          Console.WriteLine(atz);
+          }
+          }
+          
+          }
+          }
